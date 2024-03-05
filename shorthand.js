17901c773582e81d9509cb738ba670656e05e995
devlrakib <devrakib5@gmail.com>
2024-03-05 19:17:52 +0600

    first commit

---
+++ shorthand.js
@@ -0,0 +1,15 @@
+//! Variable Decleration 
+//var tomatoPrice = 20;
+//? Variable Price Update
+//tomatoPrice = 25;
+//console.log(tomatoPrice);
+//! Add Something To the Previous Value
+var price1 = 20;
+var price2 = 15;
+//? Set A New value 
+//price1 = 25;
+//! Add Some value To The Existing Value 
+price1 = price1+11;
+console.log(price1);
+
+//Addition To The Same variable Shorthand 
